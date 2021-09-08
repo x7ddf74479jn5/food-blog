702712fc917b4298b9a0c1057e72293ff1adc890
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-09-08 22:01:34 +0900

    💄 add: tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import "src/styles/global.css";
+import "tailwindcss/tailwind.css";
 
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
-const App = (props: AppProps) => {
-  return <props.Component {...props.pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
