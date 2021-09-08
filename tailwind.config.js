480fe9b7ba043ff99db7f92ac10499adab544e06
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-09-08 21:52:58 +0900

    🔧 add: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme("colors.gray.300"),
+            h1: {
+              color: theme("colors.gray.100"),
+            },
+            h2: {
+              color: theme("colors.gray.100"),
+            },
+            strong: {
+              color: theme("colors.gray.300"),
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
