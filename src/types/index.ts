d4701a6924ba0e4d953f6ce89a4387db15491185
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-09-08 21:00:38 +0900

    🏷️ add: types

---
+++ src/types/index.ts
@@ -0,0 +1,76 @@
+export type TDateCommon = {
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
+
+export type TCategory = {
+  id: string;
+  slug: string;
+  name: string;
+  color: string;
+} & TDateCommon;
+
+export type TTag = {
+  id: string;
+  slug: string;
+  name: string;
+  color: string;
+} & TDateCommon;
+
+export type TImage = {
+  url: string;
+  width: number;
+  height: number;
+};
+
+export type TImageOption = {
+  fontColor: string;
+};
+
+export type TArticle = {
+  id: string;
+  image: TImage;
+  title: string;
+  excerpt: string;
+  body: string;
+  category: TCategory;
+  tags: TTag[];
+  imageOption: TImageOption | null;
+} & TDateCommon;
+
+export type TProfile = {
+  name: string;
+  fullName: string;
+  description: string;
+  githubAccountName: string;
+  twitterAccountName: string;
+  image: TImage;
+};
+
+export type TConfig = {
+  perPage: number;
+  host: string;
+  apiHost: string;
+  siteTitle: string;
+  siteDescription: string;
+  siteKeywords: string[];
+  siteImage: TImage;
+  profile: TProfile;
+} & TDateCommon;
+
+export type TResources = "article" | "note";
+
+export type TListResponse<T> = {
+  contents: T[];
+  totalCount: number;
+  offset?: number;
+  limit?: number;
+};
+
+export type TArticleListResponse = TListResponse<TArticle>;
+
+export type TCategoryListResponse = TListResponse<TCategory>;
+
+export type TTagListResponse = TListResponse<TTag>;
