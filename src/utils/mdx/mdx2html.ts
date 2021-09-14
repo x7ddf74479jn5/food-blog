8f74a7452bf5bc90789db4ba241deea9df6f2bcf
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-09-15 01:59:56 +0900

    ✨ add: remerk plugins

---
+++ src/utils/mdx/mdx2html.ts
@@ -0,0 +1,18 @@
+import { serialize } from "next-mdx-remote/serialize";
+import remarkBreaks from "remark-breaks";
+import remarkEmoji from "remark-emoji";
+import remarkFootnotes from "remark-footnotes";
+import remarkSlug from "remark-slug";
+import remarkUnwrapImages from "remark-unwrap-images";
+
+export const mdx2html = async (source: string) => {
+  const mdxSource = await serialize(source, {
+    mdxOptions: {
+      remarkPlugins: [remarkBreaks, remarkEmoji, remarkFootnotes, remarkSlug, remarkUnwrapImages],
+    },
+  });
+
+  return mdxSource;
+};
+
+export default mdx2html;
