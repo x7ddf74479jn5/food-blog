4cf81f5c718a55dbb518c28055cfe46173811b9b
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-09-16 21:19:31 +0900

    🐛 chore

--- src/components/atoms/Spinner/index.tsx
+++ src/components/atoms/Spinner/index.tsx
@@ -1,6 +1,6 @@
 const Spinner: React.VFC = () => {
   return (
-    <div className="flex justify-center items-center w-full h-screen">
+    <div className="flex fixed inset-0 justify-center items-center w-full h-screen">
       <div className="w-32 h-32 rounded-full border-b-2 border-green-500 animate-spin"></div>
     </div>
   );
