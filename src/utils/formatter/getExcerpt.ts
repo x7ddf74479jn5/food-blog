75f940cb0b33d675ebd3463657f5f26b449729dd
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-09-17 22:37:51 +0900

    ✨ add: getExcerpt

---
+++ src/utils/formatter/getExcerpt.ts
@@ -0,0 +1,7 @@
+export const getExcerpt = (body: string) => {
+  const plane = body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "").replace(/\n/g, "");
+  const isOver = plane.length >= 100;
+  return `${plane?.slice(0, 99)}${isOver ? "…" : ""}` || "";
+};
+
+export default getExcerpt;
