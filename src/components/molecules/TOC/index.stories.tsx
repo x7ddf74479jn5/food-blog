79436b4db42af6b85d0b2f62369123c9f5e32528
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-09-30 00:26:34 +0900

    ✨ add: stories

---
+++ src/components/molecules/TOC/index.stories.tsx
@@ -0,0 +1,20 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import TOC from ".";
+
+export default {
+  title: "Molecules/TOC",
+  component: TOC,
+} as ComponentMeta<typeof TOC>;
+
+const Template: ComponentStory<typeof TOC> = (args) => (
+  <div className="flex flex-row prose">
+    <div id="js-toc-content" className="hidden">
+      <h2>h2</h2>
+      <h3>h3</h3>
+    </div>
+    <TOC {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
