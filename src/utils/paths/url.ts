a34549ed498977ee810c0540d4ab0fb825d96bb7
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-02 01:37:53 +0900

    🍬 chore

--- src/utils/paths/url.ts
+++ src/utils/paths/url.ts
@@ -1,7 +1,22 @@
+type Table = {
+  [key: string]: string;
+};
+
 export const UrlTable = {
-  root: "/",
   home: "/",
-  articles: "/articles",
+  root: "/",
   categories: "/categories",
   tags: "/tags",
+};
+
+const labelTable: Table = {
+  "/": "レシピ一覧へ",
+  "/categories": "カテゴリー別一覧へ",
+  "/tags": "タグ別一覧へ",
+};
+
+export const getBackLinks = (paths: string[]): { href: string; label: string }[] => {
+  return paths.map((path) => {
+    return { href: path, label: labelTable[path] };
+  });
 };
