e4aed96f7cc1cb66594b452911b572d5e46270e9
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-08 19:16:39 +0900

    ✨ add: TextDate

---
+++ src/components/atoms/texts/TextDate/index.stories.tsx
@@ -0,0 +1,18 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import TextDate from ".";
+
+export default {
+  title: "Atoms/TextDate",
+  component: TextDate,
+  argTypes: {
+    date: { control: { type: "date" } },
+  },
+} as ComponentMeta<typeof TextDate>;
+
+const Template: ComponentStory<typeof TextDate> = (args) => <TextDate {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  date: new Date("2020-01-01"),
+};
