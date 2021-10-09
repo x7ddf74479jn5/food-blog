41e9938cc184ae4e04fbc76286f10a7390acc51c
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-10 02:52:04 +0900

    ♻️ Refactor: Spinner and Pagination

--- src/components/atoms/Spinner/index.tsx
+++ src/components/atoms/Spinner/index.tsx
@@ -1,9 +1,7 @@
-const Spinner: React.VFC = () => {
-  return (
-    <div className="flex fixed inset-0 justify-center items-center w-full h-screen">
-      <div className="w-32 h-32 rounded-full border-b-2 border-green-500 animate-spin"></div>
-    </div>
-  );
+type SpinnerProps = { size: string };
+
+const Spinner: React.VFC<SpinnerProps> = ({ size }) => {
+  return <div className={`${size} rounded-full border-b-2 border-green-500 animate-spin`} />;
 };
 
 export default Spinner;
