6564dd328f126107d557e4483615a0fb724991d7
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-10 18:21:26 +0900

    ♻️ Refactor: Layouts

--- src/components/layouts/ArticleLayout/index.tsx
+++ src/components/layouts/ArticleLayout/index.tsx
@@ -1,8 +1,8 @@
+import dynamic from "next/dynamic";
+
 import { ShareButtons } from "@/components/atoms/ShareButtons/index";
-import BackLinks from "@/components/molecules/BackLinks";
-import Meta from "@/components/molecules/Meta";
+import { RootLayout } from "@/components/layouts/RootLayout";
 import { TOC } from "@/components/molecules/TOC/index";
-import Header from "@/components/organisms/Header";
 import { RelatedArticles } from "@/components/organisms/RelatedArticles";
 import type { TArticle, TConfig } from "@/types";
 
@@ -19,11 +19,10 @@ type Props = {
 };
 
 const ArticleLayout: React.FC<Props> = ({ url, children, config, pageTitle, backLinks, relatedArticles }: Props) => {
+  const BackLinks = dynamic(() => import("@/components/molecules/BackLinks"));
   return (
-    <>
-      <Meta />
-      <div className="px-4 mx-auto mb-16 max-w-screen-lg">
-        <Header siteTitle={config.siteTitle} />
+    <RootLayout config={config}>
+      <div className="mb-32">
         <div className="flex flex-col lg:flex-row gap-16 items-center lg:items-start mt-4 mb-8">
           <main className="pb-12">{children}</main>
           <aside className="flex lg:sticky top-3 flex-col gap-y-8 mb-4 w-full lg:w-1/3 h-full">
@@ -34,7 +33,7 @@ const ArticleLayout: React.FC<Props> = ({ url, children, config, pageTitle, back
         </div>
         <BackLinks links={backLinks} />
       </div>
-    </>
+    </RootLayout>
   );
 };
 
---
+++ src/components/layouts/RootLayout/index.tsx
@@ -0,0 +1,25 @@
+import dynamic from "next/dynamic";
+
+import Meta from "@/components/molecules/Meta";
+import Header from "@/components/organisms/Header";
+import type { TConfig } from "@/types";
+
+type RootLayoutProps = {
+  config: TConfig;
+  children: React.ReactNode;
+};
+
+export const RootLayout: React.FC<RootLayoutProps> = ({ config, children }) => {
+  const Footer = dynamic(() => import("@/components/organisms/Footer"));
+  const { siteTitle, organization } = config;
+  return (
+    <>
+      <Meta />
+      <div className="flex flex-col px-4 mx-auto mb-16 max-w-screen-lg min-h-screen">
+        <Header siteTitle={siteTitle} />
+        <div className="grid flex-1 justify-center">{children}</div>
+        <Footer siteTitle={siteTitle} organization={organization} />
+      </div>
+    </>
+  );
+};
