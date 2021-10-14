fdacdcf3e30862481de85f88c23168395e0e6e82
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-15 00:14:48 +0900

    ♻️ Refactor: layouts

--- src/components/atoms/containers/ContainerWithOrder/index.tsx
+++ src/components/atoms/containers/ContainerWithOrder/index.tsx
@@ -1,8 +1,9 @@
 type ContainerWithOrderProps = {
   children: React.ReactNode;
   order: string;
+  className?: string;
 };
 
-export const ContainerWithOrder: React.FC<ContainerWithOrderProps> = ({ order, children }) => {
-  return <div className={`flex flex-col ${order} items-center w-full`}>{children} </div>;
+export const ContainerWithOrder: React.FC<ContainerWithOrderProps> = ({ order, children, className = "" }) => {
+  return <div className={`flex flex-col ${order} items-center w-full ${className}`}>{children} </div>;
 };
