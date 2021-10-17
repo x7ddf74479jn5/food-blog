214db1030c55c953f11cb329e8abf9d0a8e1b5ff
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-17 22:37:24 +0900

    ✨ add: pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withBundleAnalyzer({
-  i18n: { locales: ["ja"], defaultLocale: "ja" },
-  reactStrictMode: true,
-  typescript: { ignoreDevErrors: true },
-  poweredByHeader: false,
-  images: {
-    domains: ["images.microcms-assets.io"],
-  },
-  pageExtensions: ["page.tsx", "page.ts"],
-});
+module.exports = withBundleAnalyzer(
+  withPWA({
+    i18n: { locales: ["ja"], defaultLocale: "ja" },
+    reactStrictMode: true,
+    typescript: { ignoreDevErrors: true },
+    poweredByHeader: false,
+    images: {
+      domains: ["images.microcms-assets.io"],
+    },
+    pageExtensions: ["page.tsx", "page.ts"],
+    pwa: {
+      dest: "public",
+      runtimeCaching,
+    },
+  })
+);
