ecd05f4499ece4028bcac7a307d00e8c6f394805
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-17 22:48:31 +0900

    🍬 chore

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,36 +3,28 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { MDXProvider } from "@mdx-js/react";
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
+import type { AppPropsWithLayout } from "next/app";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
-import type { ReactElement, ReactNode } from "react";
+import type { ReactNode } from "react";
 
 import MDXCustomComponents from "@/components/atoms/mdx";
-import { usePageView } from "@/lib/gtag";
+import { GoogleAnalytics, usePageView } from "@/lib/gtag";
 
-type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  Component: NextPageWithLayout;
-};
+import SEO from "../../next-seo.config";
 
 const App = ({ Component, pageProps }: AppPropsWithLayout): ReactNode => {
   usePageView();
 
   const getLayout = Component.getLayout ?? ((page) => page);
   return getLayout(
-    <DefaultSeo>
-      <ThemeProvider attribute="class">
-        <MDXProvider components={MDXCustomComponents}>
-          <Component {...pageProps} />
-        </MDXProvider>
-      </ThemeProvider>
-    </DefaultSeo>
+    <ThemeProvider attribute="class">
+      <MDXProvider components={MDXCustomComponents}>
+        <DefaultSeo {...SEO} />
+        <GoogleAnalytics />
+        <Component {...pageProps} />
+      </MDXProvider>
+    </ThemeProvider>
   );
 };
 
