9cc90462a7f9d206308c326a1a1b1cc046ad32c1
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 01:29:24 +0900

    🔧 update: client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -3,5 +3,4 @@ import { createClient } from "microcms-js-sdk";
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN || "",
   apiKey: process.env.X_API_KEY || "",
-  globalDraftKey: process.env.X_GLOBAL_DRAFT_KEY || "",
 });
