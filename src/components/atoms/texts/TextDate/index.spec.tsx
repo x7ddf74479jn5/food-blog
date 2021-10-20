03cb5ca5e6860c6467a9baaf91d9a9bce6256c1f
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 22:55:25 +0900

    ✅ pass: TextDate

--- src/components/atoms/texts/TextDate/index.spec.tsx
+++ src/components/atoms/texts/TextDate/index.spec.tsx
@@ -1,3 +1,4 @@
+import { render } from "jest/test-utils";
 import renderer from "react-test-renderer";
 
 import TextDate from ".";
@@ -7,5 +8,12 @@ describe("components/atoms/TextDate", () => {
   it("snapshot", () => {
     const tree = renderer.create(<TextDate date={date} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: 正しく表示されている", () => {
+    const { container } = render(<TextDate date={date} />);
+    const time = container.querySelector("time");
+    expect(time).toHaveTextContent("2020年12月12日");
+    expect(time).toHaveAttribute("dateTime", "2020-12-12T00:00:00+09:00");
   });
 });
