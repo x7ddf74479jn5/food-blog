d42040e2dc5f04312cc769286fd6c3686fd6161d
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 23:39:01 +0900

    ✅ pass: Tooltip

--- src/components/atoms/Tooltip/index.spec.tsx
+++ src/components/atoms/Tooltip/index.spec.tsx
@@ -1,3 +1,4 @@
+import { render, screen } from "jest/test-utils";
 import renderer from "react-test-renderer";
 
 import Tooltip from ".";
@@ -6,5 +7,13 @@ describe("components/atoms/Tooltip", () => {
   it("snapshot", () => {
     const tree = renderer.create(<Tooltip label="label">children</Tooltip>).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: 正しく表示されている", () => {
+    render(<Tooltip label="label">children</Tooltip>);
+    const elm = screen.getByText("children");
+    expect(elm).toBeInTheDocument();
+    const label = screen.getByText("label");
+    expect(label).toBeInTheDocument();
   });
 });
