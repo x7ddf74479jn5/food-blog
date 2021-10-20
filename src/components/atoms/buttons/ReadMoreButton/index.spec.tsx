4f789a0d55cf90a5ac518b7172e157ea90c308dc
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 20:56:14 +0900

    ✅ pass: ReadMoreButton

--- src/components/atoms/buttons/ReadMoreButton/index.spec.tsx
+++ src/components/atoms/buttons/ReadMoreButton/index.spec.tsx
@@ -1,3 +1,5 @@
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "jest/test-utils";
 import renderer from "react-test-renderer";
 
 import { ReadMoreButton } from ".";
@@ -7,5 +9,17 @@ describe("components/atoms/buttons/ReadMoreButton", () => {
   it("snapshot", () => {
     const tree = renderer.create(<ReadMoreButton handleOnClick={mockHandleClick} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: ラベルが表示されている", () => {
+    const { container } = render(<ReadMoreButton handleOnClick={mockHandleClick} />);
+    expect(container).toHaveTextContent("もっと読み込む");
+  });
+
+  it("OK: クリックイベントが発火する", () => {
+    render(<ReadMoreButton handleOnClick={mockHandleClick} />);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    expect(mockHandleClick).toBeCalled();
   });
 });
