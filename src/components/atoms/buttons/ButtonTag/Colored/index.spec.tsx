d17aa564f5e2895847e5d616c8c15e61078b59c6
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 21:13:02 +0900

    ✅ pass: ButtonTag

--- src/components/atoms/buttons/ButtonTag/Colored/index.spec.tsx
+++ src/components/atoms/buttons/ButtonTag/Colored/index.spec.tsx
@@ -1,11 +1,27 @@
-import { mockTags } from "@mocks/data/tags";
+import { render, screen } from "jest/test-utils";
+import { mockTags } from "mocks/data";
 import renderer from "react-test-renderer";
+
+import { UrlTable } from "@/utils/paths/url";
 
 import { ButtonTagColored } from ".";
 
 describe("components/atoms/buttons/ButtonTagColored", () => {
+  const tagRice = mockTags.rice;
+
   it("snapshot", () => {
-    const tree = renderer.create(<ButtonTagColored tag={mockTags.rice} />).toJSON();
+    const tree = renderer.create(<ButtonTagColored tag={tagRice} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: ラベルが表示されている", () => {
+    const { container } = render(<ButtonTagColored tag={tagRice} />);
+    expect(container).toHaveTextContent(tagRice.name);
+  });
+
+  it("OK: aタグのhrefが正しい", () => {
+    render(<ButtonTagColored tag={tagRice} />);
+    const anchor = screen.getByRole("link");
+    expect(anchor).toHaveAttribute("href", `${UrlTable.tags}/${tagRice.slug}`);
   });
 });
--- src/components/atoms/buttons/ButtonTag/Plain/index.spec.tsx
+++ src/components/atoms/buttons/ButtonTag/Plain/index.spec.tsx
@@ -1,11 +1,27 @@
-import { mockTags } from "@mocks/data/tags";
+import { render, screen } from "jest/test-utils";
+import { mockTags } from "mocks/data";
 import renderer from "react-test-renderer";
+
+import { UrlTable } from "@/utils/paths/url";
 
 import { ButtonTagPlain } from ".";
 
 describe("components/atoms/buttons/ButtonTagPlain", () => {
+  const tagRice = mockTags.rice;
+
   it("snapshot", () => {
     const tree = renderer.create(<ButtonTagPlain tag={mockTags.rice} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: ラベルが表示されている", () => {
+    const { container } = render(<ButtonTagPlain tag={tagRice} />);
+    expect(container).toHaveTextContent(tagRice.name);
+  });
+
+  it("OK: aタグのhrefが正しい", () => {
+    render(<ButtonTagPlain tag={tagRice} />);
+    const anchor = screen.getByRole("link");
+    expect(anchor).toHaveAttribute("href", `${UrlTable.tags}/${tagRice.slug}`);
   });
 });
