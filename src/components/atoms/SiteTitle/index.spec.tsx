0970c7ee103af9157f1b24b065f5cc477ae73c46
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 22:46:38 +0900

    ✅ pass: SiteTitle

--- src/components/atoms/SiteTitle/index.spec.tsx
+++ src/components/atoms/SiteTitle/index.spec.tsx
@@ -1,4 +1,5 @@
 import { mockConfig } from "@mocks/data/config";
+import { render, screen } from "jest/test-utils";
 import renderer from "react-test-renderer";
 
 import { SiteTitle } from ".";
@@ -9,5 +10,12 @@ describe("components/atoms/SiteTitle", () => {
   it("snapshot", () => {
     const tree = renderer.create(<SiteTitle size="text-2xl" title={siteTitle} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: 正しく表示されている", () => {
+    render(<SiteTitle size="text-2xl" title={siteTitle} />);
+    const link = screen.getByRole("link");
+    expect(link).toHaveTextContent(siteTitle);
+    expect(link).toHaveAttribute("href", "/");
   });
 });
