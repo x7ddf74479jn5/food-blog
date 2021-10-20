73cd1f131896db2ef36ef7f7cf0b86d73fa15c98
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 21:40:48 +0900

    ✅ pass: CustomImage

--- src/components/atoms/mdx/CustomImage/index.spec.tsx
+++ src/components/atoms/mdx/CustomImage/index.spec.tsx
@@ -3,12 +3,12 @@ import renderer from "react-test-renderer";
 import CustomImage from ".";
 
 describe("components/atoms/mdx/CustomImage", () => {
+  const mockImage = "/mocks/data/images/2996666_s.jpg";
+
   it("snapshot", () => {
-    const image = renderer.create(<CustomImage src={"/mocks/data/images/2996666_s.jpg"} />).toJSON();
-    expect(image).toMatchSnapshot();
-    const nextImage = renderer
-      .create(<CustomImage src={"/mocks/data/images/2996666_s.jpg"} width={100} height={100} />)
-      .toJSON();
-    expect(nextImage).toMatchSnapshot();
+    const image1 = renderer.create(<CustomImage src={mockImage} />).toJSON();
+    expect(image1).toMatchSnapshot();
+    const image2 = renderer.create(<CustomImage src={mockImage} width={100} height={100} />).toJSON();
+    expect(image2).toMatchSnapshot();
   });
 });
