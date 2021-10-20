bbb44900c3040ea7b624eb336ec4c5b2f5b57ac3
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 21:06:21 +0900

    ✅ pass: ButtonCategory

--- src/components/atoms/buttons/ButtonCategory/index.spec.tsx
+++ src/components/atoms/buttons/ButtonCategory/index.spec.tsx
@@ -1,11 +1,26 @@
+import { render, screen } from "jest/test-utils";
 import { mockCategories } from "mocks/data/categories";
 import renderer from "react-test-renderer";
+
+import { UrlTable } from "@/utils/paths/url";
 
 import ButtonCategory from ".";
 
 describe("components/atoms/buttons/ButtonCategory", () => {
+  const categoryRice = mockCategories.rice;
   it("snapshot", () => {
-    const tree = renderer.create(<ButtonCategory category={mockCategories.rice} />).toJSON();
+    const tree = renderer.create(<ButtonCategory category={categoryRice} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: ラベルが表示されている", () => {
+    const { container } = render(<ButtonCategory category={categoryRice} />);
+    expect(container).toHaveTextContent(categoryRice.name);
+  });
+
+  it("OK: aタグのhrefが正しい", () => {
+    render(<ButtonCategory category={categoryRice} />);
+    const anchor = screen.getByRole("link");
+    expect(anchor).toHaveAttribute("href", `${UrlTable.categories}/${categoryRice.slug}`);
   });
 });
