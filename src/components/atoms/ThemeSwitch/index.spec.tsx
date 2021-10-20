ad6416302eaed1f01e261f75ea0b45bc6ec326cf
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-20 23:48:57 +0900

    ✅ pass: ThemeSwitch

--- src/components/atoms/ThemeSwitch/index.spec.tsx
+++ src/components/atoms/ThemeSwitch/index.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from "jest/test-utils";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "jest/test-utils";
 
 import ThemeSwitch from ".";
 
@@ -6,5 +7,13 @@ describe("components/atoms/ThemeSwitch", () => {
   it("snapshot", () => {
     const { asFragment } = render(<ThemeSwitch />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("OK: スイッチが正常に切り替わる", () => {
+    render(<ThemeSwitch />);
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).not.toBeChecked();
+    userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
   });
 });
