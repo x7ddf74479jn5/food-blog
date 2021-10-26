5881a33657247142ce4392200d848a45f0575d47
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-26 22:13:03 +0900

    ✅ pass: Header

---
+++ src/components/organisms/Header/index.spec.tsx
@@ -0,0 +1,25 @@
+import { mockConfig } from "@mocks/data";
+import { render, screen } from "jest/test-utils";
+import renderer from "react-test-renderer";
+
+import Header from "../Header";
+
+describe("components/molecules/Header", () => {
+  const { siteTitle } = mockConfig;
+  it("snapshot", () => {
+    const tree = renderer.create(<Header siteTitle={siteTitle} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("OK: 初期表示が正しい", () => {
+    render(<Header siteTitle={siteTitle} />);
+    const header = screen.getByRole("banner");
+    expect(header).toBeInTheDocument();
+    expect(header.tagName).toBe("HEADER");
+    const anchor = screen.getByRole("link");
+    expect(anchor).toHaveTextContent(siteTitle);
+    expect(anchor).toHaveAttribute("href", "/");
+    const searchBoxes = screen.getAllByRole("searchbox");
+    expect(searchBoxes.length).toBe(2);
+  });
+});
