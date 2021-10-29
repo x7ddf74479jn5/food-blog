cb18b208255411055e8b07dbc020dd46a45cd11a
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-30 02:28:36 +0900

    ✅ pass: useGetArticleListQuery

---
+++ src/hooks/useGetArticleListQuery/index.spec.tsx
@@ -0,0 +1,228 @@
+import { mockArticles } from "@mocks/data";
+import type { RenderResult } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
+import { withMockedRouter } from "jest/test-utils";
+import type { NextRouter } from "next/router";
+import { SWRConfig } from "swr";
+import * as swr from "swr";
+import * as useSWRInfinite from "swr/infinite";
+
+import { UrlTable } from "@/utils/paths/url";
+
+import useGetArticleListQuery from ".";
+
+let spyUseSWRInfinite: jest.SpyInstance;
+let spyMutate: jest.SpyInstance;
+
+beforeAll(() => {
+  spyUseSWRInfinite = jest.spyOn(useSWRInfinite, "default");
+  spyMutate = jest.spyOn(swr, "mutate");
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("hooks/useGetArticleListQuery", () => {
+  const q = "作り方";
+  const router: Partial<NextRouter> = {
+    query: { q },
+  };
+  const Wrapper: React.ComponentType<{ children: React.ReactNode; router: Partial<NextRouter> }> = ({ children }) => {
+    return withMockedRouter(
+      router,
+      <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>{children}</SWRConfig>
+    );
+  };
+  let renderResult: RenderResult<ReturnType<typeof useGetArticleListQuery>>;
+  const mockArticleList = Object.values(mockArticles);
+
+  it("OK: useSWRInfiniteが呼び出される", () => {
+    const limit = 2;
+
+    renderHook(() => useGetArticleListQuery({ perPage: limit }), {
+      wrapper: Wrapper,
+    });
+
+    expect(spyUseSWRInfinite).toBeCalledTimes(1);
+  });
+
+  it("OK: APIの結果が正しい", () => {
+    const limit = 2;
+    const mockSetSize = jest.fn();
+    const offset = 0;
+
+    spyUseSWRInfinite.mockImplementationOnce((getKey) => {
+      let size = 0;
+      getKey(size, null);
+      size++;
+
+      return {
+        size,
+        setSize: mockSetSize,
+        data: [
+          {
+            contents: mockArticleList.slice(offset, offset + limit),
+            totalCount: mockArticleList.length,
+            offset,
+            limit,
+          },
+        ],
+      };
+    });
+
+    const { result } = renderHook(() => useGetArticleListQuery({ perPage: limit }), {
+      wrapper: Wrapper,
+    });
+    renderResult = result;
+    const { getCurrentKey, size, articles, hasNextPage, paginate, revalidate } = renderResult.current;
+
+    expect(spyUseSWRInfinite).toBeCalledTimes(1);
+    const key = getCurrentKey();
+    const expectedKey = `${UrlTable.apiSearch}?q=${q}&limit=${limit}&pageIndex=0&previousPageData=null`;
+    expect(key).toBe(expectedKey);
+
+    expect(articles).toStrictEqual(mockArticleList.slice(0, 2));
+    expect(hasNextPage).toBeTruthy();
+    expect(size).toBe(1);
+
+    paginate();
+    expect(mockSetSize).toBeCalledWith(2);
+
+    revalidate();
+    expect(spyMutate).toHaveBeenCalledWith(expectedKey);
+  });
+
+  describe("OK: keyとarticlesの対応が正しい", () => {
+    it("1st: 取得数/総取得数/総記事数 2/2/4", () => {
+      const limit = 2;
+      const mockSetSize = jest.fn();
+      const offset = 0;
+
+      spyUseSWRInfinite.mockImplementationOnce((getKey) => {
+        let size = 0;
+        getKey(size, null);
+        size++;
+
+        return {
+          size,
+          setSize: mockSetSize,
+          data: [
+            {
+              contents: mockArticleList.slice(0, 2),
+              totalCount: mockArticleList.length,
+              offset,
+              limit,
+            },
+          ],
+        };
+      });
+
+      const { result } = renderHook(() => useGetArticleListQuery({ perPage: limit }), {
+        wrapper: Wrapper,
+      });
+      renderResult = result;
+      const { getCurrentKey, size, articles, hasNextPage } = renderResult.current;
+
+      const key = getCurrentKey();
+      const expectedKey = `${UrlTable.apiSearch}?q=${q}&limit=${limit}&pageIndex=0&previousPageData=null`;
+      expect(key).toBe(expectedKey);
+      expect(articles).toStrictEqual(mockArticleList.slice(0, 2));
+      expect(hasNextPage).toBeTruthy();
+      expect(size).toBe(1);
+    });
+
+    it("2nd: 取得数/総取得数/総記事数 2/4/4", () => {
+      const limit = 2;
+      const mockSetSize = jest.fn();
+      let offset = 0;
+      spyUseSWRInfinite.mockImplementation((getKey) => {
+        let size = 1;
+        getKey(size, { contents: mockArticleList.slice(0, 2), offset, limit });
+        size++;
+        offset += limit;
+
+        return {
+          size,
+          setSize: mockSetSize,
+          data: [
+            {
+              contents: mockArticleList.slice(0, 2),
+              totalCount: mockArticleList.length,
+              offset,
+              limit,
+            },
+            {
+              contents: mockArticleList.slice(2, 4),
+              totalCount: mockArticleList.length,
+              offset,
+              limit,
+            },
+          ],
+        };
+      });
+
+      const { result } = renderHook(() => useGetArticleListQuery({ perPage: limit }), {
+        wrapper: Wrapper,
+      });
+      renderResult = result;
+      const { getCurrentKey, size, articles, hasNextPage } = renderResult.current;
+
+      const key = getCurrentKey();
+      const expectedKey = `${UrlTable.apiSearch}?q=${q}&offset=${offset}&limit=${limit}&pageIndex=1&previousPageData=[object Object]`;
+      expect(key).toBe(expectedKey);
+      expect(articles).toStrictEqual(mockArticleList.slice(0, 4));
+      expect(hasNextPage).toBeFalsy();
+      expect(size).toBe(2);
+    });
+
+    it("3rd 取得数/総取得数/総記事数 0/4/4", () => {
+      const limit = 2;
+      const mockSetSize = jest.fn();
+      let offset = 2;
+      spyUseSWRInfinite.mockImplementation((getKey) => {
+        const size = 2;
+        getKey(size, { contents: mockArticleList.slice(2, 4), offset, limit });
+        offset += limit;
+
+        return {
+          size,
+          setSize: mockSetSize,
+          data: [
+            {
+              contents: mockArticleList.slice(0, 2),
+              totalCount: mockArticleList.length,
+              offset: 0,
+              limit,
+            },
+            {
+              contents: mockArticleList.slice(2, 4),
+              totalCount: mockArticleList.length,
+              offset: 2,
+              limit,
+            },
+            {
+              contents: [],
+              totalCount: mockArticleList.length,
+              offset: 4,
+              limit,
+            },
+          ],
+        };
+      });
+
+      const { result } = renderHook(() => useGetArticleListQuery({ perPage: limit }), {
+        wrapper: Wrapper,
+      });
+      renderResult = result;
+      const { getCurrentKey, size, articles, hasNextPage } = renderResult.current;
+
+      const key = getCurrentKey();
+      const expectedKey = `${UrlTable.apiSearch}?q=${q}&offset=${offset}&limit=${limit}&pageIndex=2&previousPageData=[object Object]`;
+      expect(key).toBe(expectedKey);
+      expect(articles).toStrictEqual(mockArticleList.slice(0, 4));
+      expect(hasNextPage).toBeFalsy();
+      expect(size).toBe(2);
+    });
+  });
+});
--- src/hooks/useGetArticleListQuery/index.ts
+++ src/hooks/useGetArticleListQuery/index.ts
@@ -17,17 +17,24 @@ const useGetArticleListQuery = ({ perPage = 4, options }: Arguments) => {
   const q = router.query.q;
   const keyRef = useRef("");
 
+  // if (!q) return;
+
   const getKey = (pageIndex: number, previousPageData: TArticleSWRResponse) => {
     let key;
+
     if (previousPageData && !previousPageData.contents) return null;
+
     if (pageIndex === 0) {
       key = `${UrlTable.apiSearch}?q=${q}&limit=${perPage}&pageIndex=${pageIndex}&previousPageData=${previousPageData}`;
       keyRef.current = key;
+
       return key;
     }
+
     const offset = previousPageData?.offset !== undefined ? previousPageData.offset + perPage : 0;
     key = `${UrlTable.apiSearch}?q=${q}&offset=${offset}&limit=${perPage}&pageIndex=${pageIndex}&previousPageData=${previousPageData}`;
     keyRef.current = key;
+
     return key;
   };
 
@@ -40,6 +47,8 @@ const useGetArticleListQuery = ({ perPage = 4, options }: Arguments) => {
     ...options,
   });
 
+  const { size, setSize, data } = result;
+
   const getCurrentKey = () => {
     return keyRef.current;
   };
@@ -48,7 +57,15 @@ const useGetArticleListQuery = ({ perPage = 4, options }: Arguments) => {
     mutate(keyRef.current);
   };
 
-  return { ...result, getCurrentKey, revalidate } as const;
+  const paginate = () => {
+    setSize(size + 1);
+  };
+
+  const articles = data ? data.map((r) => (r ? r.contents : [])).flat() : [];
+  const totalCount = data ? data[0]?.totalCount : 0;
+  const hasNextPage = totalCount ? articles.length !== totalCount : false;
+
+  return { ...result, getCurrentKey, revalidate, paginate, hasNextPage, articles } as const;
 };
 
 export default useGetArticleListQuery;
