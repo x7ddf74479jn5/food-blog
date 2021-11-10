9aa2159246c5bf463022b5a3be5a06a3fcaf5e3b
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-11-10 21:47:43 +0900

    💄 update: AsideContainer

--- src/components/atoms/containers/AsideContainer/index.tsx
+++ src/components/atoms/containers/AsideContainer/index.tsx
@@ -1,12 +1,15 @@
 type AsideContainerProps = {
   children: React.ReactNode;
+  className?: string;
   side: "left" | "right";
 };
 
-export const AsideContainer: React.FC<AsideContainerProps> = ({ children, side }) => {
+export const AsideContainer: React.FC<AsideContainerProps> = ({ children, side, className }) => {
   const _side = side === "left" ? "order-2 md:order-1 " : "order-3";
   return (
-    <aside className={`flex flex-col flex-1 ${_side} gap-y-8 items-center md:mt-16 w-full  min-w-max lg:w-1/3 h-auto`}>
+    <aside
+      className={`flex flex-col flex-1 ${_side} gap-y-8 items-center md:mt-16 w-full  min-w-max lg:w-1/3 h-auto ${className}`}
+    >
       <div className="flex flex-col h-full">
         <div className="flex md:sticky md:top-16">{children}</div>
       </div>
