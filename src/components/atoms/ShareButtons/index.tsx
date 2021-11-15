48e61adcbd3db9ae45c7969f87d3b13bf08f7420
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-11-15 21:53:25 +0900

    🩹 chore

--- src/components/atoms/ShareButtons/index.tsx
+++ src/components/atoms/ShareButtons/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable tailwindcss/classnames-order */
 import { FaFacebook, FaGetPocket, FaLine, FaTwitter } from "react-icons/fa";
 import { SiHatenabookmark } from "react-icons/si";
 import {
@@ -23,27 +24,27 @@ export const ShareButtons: React.FC<Props> = ({ url, title, twitterId, direction
     <div className={`flex ${_direction} gap-4 justify-center items-center`}>
       <TwitterShareButton url={url} title={title} via={twitterId}>
         <Tooltip label="Twitterでシェア">
-          <FaTwitter className="hover:text-[#1DA1F2] text-gray-400" size={24} />
+          <FaTwitter className="text-gray-400 hover:text-[#1DA1F2]" size={24} />
         </Tooltip>
       </TwitterShareButton>
       <FacebookShareButton url={url} title={title}>
         <Tooltip label="Facebookでシェア">
-          <FaFacebook className="hover:text-[#3b5998] text-gray-400" size={24} />
+          <FaFacebook className="text-gray-400 hover:text-[#3b5998]" size={24} />
         </Tooltip>
       </FacebookShareButton>
       <LineShareButton url={url} title={title}>
         <Tooltip label="LINEでシェア">
-          <FaLine className="hover:text-[#00B900] text-gray-400" size={24} />
+          <FaLine className="text-gray-400 hover:text-[#00B900]" size={24} />
         </Tooltip>
       </LineShareButton>
       <PocketShareButton url={url} title={title}>
         <Tooltip label="Pocketに保存">
-          <FaGetPocket className="hover:text-[#ee4056] text-gray-400" size={24} />
+          <FaGetPocket className="text-gray-400 hover:text-[#ee4056]" size={24} />
         </Tooltip>
       </PocketShareButton>
       <HatenaShareButton url={url} title={title}>
         <Tooltip label="はてなブックマークでシェア">
-          <SiHatenabookmark className="hover:text-[#00A4DE] text-gray-400" size={24} />
+          <SiHatenabookmark className="text-gray-400 hover:text-[#00A4DE]" size={24} />
         </Tooltip>
       </HatenaShareButton>
     </div>
