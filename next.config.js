ae2a3dab55a96613cbddd8105a4a55abb1674748
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-10 21:50:58 +0900

    ⚗️ experiment: PWA

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ module.exports = withBundleAnalyzer(
       disable: process.env.NODE_ENV === "development",
       dest: "public",
       runtimeCaching,
+      buildExcludes: [/middleware-manifest.json$/],
     },
   })
 );
--- src/pages/_offline/index.page.tsx
+++ src/pages/_offline/index.page.tsx
@@ -1,5 +1,26 @@
-import Page, { getStaticProps as _getStaticProps } from "../index.page";
+import type { InferGetStaticPropsType, NextPage } from "next";
+import { FaRegTired } from "react-icons/fa";
+
+import { HeadingOne } from "@/components/atoms/texts/Heading";
+import { HtmlHeadNoIndex } from "@/components/functions/meta";
+import { RootLayout } from "@/components/layouts/RootLayout";
+
+import { getStaticProps as _getStaticProps } from "../_error/index.page";
+
+type OfflineProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Offline: NextPage<OfflineProps> = ({ config }) => (
+  <RootLayout config={config}>
+    <HtmlHeadNoIndex />
+    <div className="flex flex-col gap-12 justify-center items-center mt-8" role="alert">
+      <FaRegTired className="w-32 h-32 text-gray-500" />
+      <div className="flex flex-col gap-8 mx-auto text-center">
+        <HeadingOne>オフラインページ</HeadingOne>
+        <p>インターネットに接続のうえ、ご利用ください。</p>
+      </div>
+    </div>
+  </RootLayout>
+);
 
 export const getStaticProps = _getStaticProps;
-
-export default Page;
+export default Offline;
