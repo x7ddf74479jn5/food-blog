ab2ea0db1f6ecb4fb83d1795a3df4c012d5b9d8e
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-10 18:49:15 +0900

    🔧 update: jest config

--- jest.config.js
+++ jest.config.js
@@ -10,11 +10,7 @@ const createJestConfig = nextJest({ dir: "./" });
 const customJestConfig = {
   testEnvironment: "jsdom",
   verbose: true,
-  // globalSetup: "<rootDir>/jest/setupEnv.ts",
-  // setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.js"],
-  // modulePathIgnorePatterns: ["<rootDir>/e2e/"],
-  // testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-  // transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
+  setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.js"],
   moduleNameMapper: {
     "src/(.*)": "<rootDir>/src/$1",
     "mocks/(.*)": "<rootDir>/mocks/$1",
@@ -26,58 +22,3 @@ const customJestConfig = {
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
-
-// // @ts-check
-
-// /**
-//  * @type {import('@jest/types').Config.InitialOptions}s
-//  **/
-// module.exports = {
-//   testEnvironment: "jsdom",
-//   collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
-//   moduleNameMapper: {
-//     // Handle CSS imports (with CSS modules): https://jestjs.io/docs/webpack#mocking-css-modules
-//     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-//     // Handle CSS imports (without CSS modules)
-//     "^.+\\.(css|sass|scss)$": "<rootDir>/jest/mockStyle.js",
-//     // Handle image imports: https://jestjs.io/docs/webpack#handling-static-assets
-//     "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": `<rootDir>/jest/mockFile.js`,
-//     // Absolute Imports and Module Path Aliases
-//     "src/(.*)": "<rootDir>/src/$1",
-//     "jest/test-utils": "<rootDir>/jest/test-utils",
-//     "mocks/(.*)": "<rootDir>/mocks/$1",
-//     "@/components/(.*)": "<rootDir>/src/components/$1",
-//     "@/hooks/(.*)": "<rootDir>/src/hooks/$1",
-//     "@/context": "<rootDir>/src/context",
-//     "@/utils/(.*)": "<rootDir>/src/utils/$1",
-//     "@/lib/(.*)": "<rootDir>/src/lib/$1",
-//     "@/pages/(.*)": "<rootDir>/src/pages/$1",
-//   },
-//   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-//   transform: {
-//     "^.+\\.(js|jsx|ts|tsx)$": [
-//       "@swc/jest",
-//       {
-//         module: {
-//           type: "commonjs",
-//         },
-//         jsc: {
-//           parser: {
-//             syntax: "typescript",
-//             tsx: true,
-//           },
-//           transform: {
-//             react: {
-//               runtime: "automatic",
-//             },
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
-//   globalSetup: "<rootDir>/jest/setupEnv.ts",
-//   setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.js"],
-//   snapshotResolver: "<rootDir>/jest/jest.snapshot.js",
-//   verbose: true,
-// };
