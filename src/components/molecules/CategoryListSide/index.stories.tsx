0cebb2375d53efd369f817e16af1881e3e9b77c2
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-04-09 20:08:17 +0900

    📚 fix: CategoryListSide stories

--- src/components/molecules/CategoryListSide/index.stories.tsx
+++ src/components/molecules/CategoryListSide/index.stories.tsx
@@ -8,14 +8,16 @@ export default {
   component: CategoryListSide,
 } as ComponentMeta<typeof CategoryListSide>;
 
-const Template: ComponentStory<typeof CategoryListSide> = (args) => (
-  <div className="w-48 h-48">
-    <CategoryListSide {...args} />
-  </div>
-);
+const Template: ComponentStory<typeof CategoryListSide> = (args) => <CategoryListSide {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   categories: [mockCategories.rice, mockCategories.salad, mockCategories.tips],
   columns: "grid-cols-3 sm:grid-cols-5 md:grid-cols-1",
+};
+
+export const ArticlePage = Template.bind({});
+ArticlePage.args = {
+  ...Default.args,
+  columns: "grid-cols-3 sm:grid-cols-5 md:grid-cols-3",
 };
