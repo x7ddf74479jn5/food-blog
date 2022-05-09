0bf1d8000eadddd073652bc25c64c0ee50be52be
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-10 01:41:06 +0900

    🩹 chore

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -1,13 +1,30 @@
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
-import type { ReactElement, ReactNode } from "react";
+// import type { NextPage } from "next";
+// import type { AppProps } from "next/app";
+// import type { ReactElement, ReactNode } from "react";
 
-declare module "next/app" {
-  type NextPageWithLayout = NextPage & {
-    getLayout?: (page: ReactElement) => ReactNode;
-  };
-  type AppPropsWithLayout = AppProps & {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Component: NextPageWithLayout;
-  };
-}
+// declare module "next/app" {
+//   type NextPageWithLayout = NextPage & {
+//     getLayout?: (page: ReactElement) => ReactNode;
+//   };
+//   type AppPropsWithLayout = AppProps & {
+//     // eslint-disable-next-line @typescript-eslint/naming-convention
+//     Component: NextPageWithLayout;
+//   };
+// }
+
+// import type { NextPage } from "next";
+// import type { AppProps } from "next/app";
+
+// type PageAttributes = { getLayout?: (page: ReactElement) => JSX.Element };
+
+// declare module "next" {
+//   type CustomLayout = NonNullable<PageAttributes["getLayout"]>;
+//   type CustomNextPage<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & PageAttributes;
+// }
+
+// declare module "next/app" {
+//   type CustomAppPage<P = Record<string, unknown>> = (
+//     // eslint-disable-next-line @typescript-eslint/naming-convention
+//     props: AppProps<P> & { Component: NextPage & PageAttributes }
+//   ) => JSX.Element;
+// }
