6a77e6993a87b39370f2d9908292017ac6270f95
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-10 01:41:49 +0900

    ♻️ refactor

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,24 +2,20 @@ import "@/styles/global.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-// import { MDXProvider } from "@mdx-js/react";
-import type { AppPropsWithLayout } from "next/app";
+import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
-import type { ReactNode } from "react";
 
-// import MDXCustomComponents from "@/components/atoms/mdx";
 import { SearchHistoryProvider } from "@/context/SearchHistoryContext";
 import { GoogleAnalytics, usePageView } from "@/lib/gtag";
 
 import SEO from "../../next-seo.config";
 
-const App = ({ Component, pageProps }: AppPropsWithLayout): ReactNode => {
+const App = ({ Component, pageProps }: AppProps) => {
   usePageView();
 
-  const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(
-    <>
+  return (
+    <div>
       <DefaultSeo {...SEO} />
       <GoogleAnalytics />
       <ThemeProvider attribute="class">
@@ -27,7 +23,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout): ReactNode => {
           <Component {...pageProps} />
         </SearchHistoryProvider>
       </ThemeProvider>
-    </>
+    </div>
   );
 };
 
