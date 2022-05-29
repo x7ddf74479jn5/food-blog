762072025c7f57c39b081226f346b45a1c5b312c
x7ddf74479jn5 <68179184+x7ddf74479jn5@users.noreply.github.com>
2022-05-29 15:09:45 +0000

    Deploying to gh-pages from @ x7ddf74479jn5/food-blog@172b8b1ecf5784c39ca2ec2cf5854a51eebd2cde 🚀

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { mockArticles } from "./articles";
-export { mockCategories } from "./categories";
-export { mockConfig } from "./config";
-export { mockPickup } from "./pickup";
-export { mockTags } from "./tags";
-export { dateCommon } from "./utils";
-export { mockWriters } from "./writers";
