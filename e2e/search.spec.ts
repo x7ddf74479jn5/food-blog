5481bbd5f94e97284aaedbb9867aea0bb56ad780
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-01 01:54:56 +0900

    ✅  fix: e2e

--- e2e/search.spec.ts
+++ e2e/search.spec.ts
@@ -5,14 +5,14 @@ import { mockArticles, mockConfig } from "../mocks/data";
 const { stock } = mockArticles;
 
 test("search", async ({ page }) => {
-  // Go to http://localhost:3000/
-  await page.goto("https://food-blog-chi.vercel.app/");
+  // Go to staging URL
+  await page.goto("https://food-blog-git-develop-x7ddf74479jn5.vercel.app/");
   // Fill input[placeholder="Search\.\.\."]
   await page.locator("input[placeholder='Search\\.\\.\\.'] >> nth=0").fill(stock.title);
 
   await Promise.all([
     page.locator("input[placeholder='Search\\.\\.\\.'] >> nth=0").press("Enter"),
-    page.waitForNavigation(/*{ url: 'http://localhost:3000/search?q=基本の一番だしの作り方' }*/),
+    page.waitForNavigation(/*{ url: 'https://food-blog-git-develop-x7ddf74479jn5.vercel.app//search?q=基本の一番だしの作り方' }*/),
   ]);
 
   await expect(page.locator("h1")).toContainText(stock.title);
@@ -24,10 +24,10 @@ test("search", async ({ page }) => {
   // Go to the article page
   await Promise.all([
     page.locator("a", { hasText: stock.title }).first().click(),
-    page.waitForNavigation(/*{ url: 'http://localhost:3000/articles/p97vmuno3jdn' }*/),
+    page.waitForNavigation(/*{ url: 'https://food-blog-git-develop-x7ddf74479jn5.vercel.app//articles/p97vmuno3jdn' }*/),
   ]);
 
   await expect(page.locator("h1")).toHaveText(stock.title);
-  await expect(page).toHaveURL(`https://food-blog-chi.vercel.app/articles/${stock.id}`);
+  await expect(page).toHaveURL(`https://food-blog-git-develop-x7ddf74479jn5.vercel.app/articles/${stock.id}`);
   await expect(page).toHaveTitle(`${stock.title} | ${mockConfig.siteTitle}`);
 });
