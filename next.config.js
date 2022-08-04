e8647a245e21e694bdb533a09efa608eb02ae134
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-04 22:49:48 +0900

    🔧 update Fixes #2

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ module.exports = withBundleAnalyzer(
   withPWA({
     i18n: { locales: ["ja"], defaultLocale: "ja" },
     reactStrictMode: true,
-    typescript: { ignoreDevErrors: true },
     poweredByHeader: false,
     images: {
       domains: ["images.microcms-assets.io"],
