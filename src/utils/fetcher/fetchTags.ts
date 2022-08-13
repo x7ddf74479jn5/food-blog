240feb77f807410c6a05b015d93080517c642704
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-13 22:48:51 +0900

    🚚 move(#21): microcms

--- src/utils/fetcher/fetchTags.ts
+++ src/utils/fetcher/fetchTags.ts
@@ -1,4 +1,4 @@
-import { client } from "@/lib/client";
+import { client } from "@/lib/microcms";
 import type { TTag, TTagListResponse } from "@/types";
 import { HttpError } from "@/utils/error/Http";
 
