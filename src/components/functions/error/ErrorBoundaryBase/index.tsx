1f2c91b92cb7fd487bf80bb89ee0d070b12d9d34
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-15 02:55:37 +0900

    ✨ feat(#1): sentry logger

--- src/components/functions/error/ErrorBoundaryBase/index.tsx
+++ src/components/functions/error/ErrorBoundaryBase/index.tsx
@@ -1,12 +1,18 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import type { FallbackProps } from "react-error-boundary";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { ErrorFallback } from "@/components/organisms/ErrorFallback";
+import { sentryLog } from "@/lib/sentry";
 
 const FallbackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
   const handleReset = () => resetErrorBoundary();
-  console.error(error);
+
+  useEffect(() => {
+    if (error) {
+      sentryLog(error);
+    }
+  }, [error]);
 
   return (
     <ErrorFallback heading="Something went wrong" message="サイト上で問題が発生しました。" onReset={handleReset} />
@@ -16,7 +22,7 @@ const FallbackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
 export const ErrorBoundaryBase: React.FC<{ children: React.ReactNode; callback?: () => void }> = memo(
   ({ children, callback }) => {
     const handleReset = () => {
-      callback && callback();
+      callback?.();
     };
 
     return (
