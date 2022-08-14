e08c2f1588c0082ef979d66c840f1366d564ffbc
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-14 13:22:32 +0900

    🔧  setup(#1): sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer =
   process.env.ANALYZE === "true" ? require("@next/bundle-analyzer")({ enabled: true }) : (config) => config;
 const withPWA = require("next-pwa");
@@ -6,24 +7,33 @@ const runtimeCaching = require("next-pwa/cache");
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withBundleAnalyzer(
-  withPWA({
-    i18n: { locales: ["ja"], defaultLocale: "ja" },
-    reactStrictMode: true,
-    poweredByHeader: false,
-    images: {
-      domains: ["images.microcms-assets.io"],
-    },
-    pageExtensions: ["page.tsx", "page.ts"],
-    pwa: {
-      disable: process.env.NODE_ENV !== "production",
-      dest: "public",
-      runtimeCaching,
-      buildExcludes: [/middleware-manifest.json$/],
-    },
-    swcMinify: true,
-    experimental: {
-      scrollRestoration: true,
-    },
-  })
-);
+const nextConfig = {
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    domains: ["images.microcms-assets.io"],
+  },
+  pageExtensions: ["page.tsx", "page.ts"],
+  pwa: {
+    disable: process.env.NODE_ENV !== "production",
+    dest: "public",
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+  swcMinify: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+};
+
+const withSentryConfig = (config) => {
+  // withBundleAnalyzer とのバッティング回避
+  if (process.env.ANALYZE === "true") return config;
+  return require("@sentry/nextjs").withSentryConfig(config, {
+    silent: false,
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+  });
+};
+
+module.exports = withPlugins([withSentryConfig, withBundleAnalyzer, withPWA], nextConfig);
