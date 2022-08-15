235d4afe013333853a159533693fd60e908a3e46
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-15 09:49:40 +0900

    🩹 chore

--- src/pages/404/index.page.tsx
+++ src/pages/404/index.page.tsx
@@ -1,13 +1,35 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 
+import { fetchCategories, fetchConfig } from "@/api";
 import { HtmlHeadNoIndex } from "@/components/functions/meta";
+import { RootLayout } from "@/components/layouts/RootLayout";
 import { ErrorFallback } from "@/components/organisms/ErrorFallback";
+import type { TCategory, TConfig } from "@/types";
 
-const Error404Page: NextPage = () => (
-  <>
+type Error404Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Error404Page: NextPage<Error404Props> = ({ config, categories }) => (
+  <RootLayout config={config} categories={categories}>
     <HtmlHeadNoIndex />
     <ErrorFallback heading="404 - Not Found" message="ページが見つかりませんでした" />
-  </>
+  </RootLayout>
 );
 
 export default Error404Page;
+
+type StaticProps = {
+  config: TConfig;
+  categories: TCategory[];
+};
+
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
+  const config = await fetchConfig();
+  const categories = await fetchCategories();
+
+  return {
+    props: {
+      config,
+      categories,
+    },
+  };
+};
--- src/pages/404/index.spec.tsx
+++ src/pages/404/index.spec.tsx
@@ -1,10 +1,13 @@
+import { mockCategories, mockConfig } from "@mocks/data";
 import { render, screen } from "jest/test-utils";
 
 import ErrorPage from "./index.page";
 
 describe("pages/404", () => {
+  const mockCategoryList = Object.values(mockCategories);
+
   it("OK: 初期レンダリング", async () => {
-    render(<ErrorPage />);
+    render(<ErrorPage config={mockConfig} categories={mockCategoryList} />);
     const h1 = screen.getByRole("heading", { level: 1 });
     expect(h1).toHaveTextContent("404 - Not Found");
     expect(screen.getByText("ページが見つかりませんでした")).toBeInTheDocument();
