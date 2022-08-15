d2c998e2c379c695ad2ad3c2554c015f33371023
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-15 14:46:13 +0900

    ✅ fix: api tests

--- src/api/fetchConfig.spec.ts
+++ src/api/fetchConfig.spec.ts
@@ -6,11 +6,7 @@ import { fetchConfig } from ".";
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 
-// FIXME: sdkとnextをアップグレードしたら壊れた
-// serviceDomain or endpoint may be wrong.
-// Details: FetchError: request to https://food-blog.microcms.io/api/v1/configs failed, reason: Invalid character in header field name
-
-describe.skip("utils/fetcher/fetchConfig", () => {
+describe("utils/fetcher/fetchConfig", () => {
   describe("fetchConfig", () => {
     it("OK: 取得", async () => {
       const config = await fetchConfig();
