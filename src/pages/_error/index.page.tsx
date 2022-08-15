1f0d8aae87dfce1798d9ddd669e0f67cfaeed28b
Pandashark <68179184+x7ddf74479jn5@users.noreply.github.com>
2022-08-15 10:36:39 +0900

    Revert "Revert "🔥 remove: sentry scaffolded code""

--- src/pages/_error/index.page.tsx
+++ src/pages/_error/index.page.tsx
@@ -1,34 +1,46 @@
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import * as Sentry from "@sentry/nextjs";
+import type { NextPage } from "next";
 
-import { fetchCategories, fetchConfig } from "@/api";
 import { HtmlHeadNoIndex } from "@/components/functions/meta";
-import { RootLayout } from "@/components/layouts/RootLayout";
 import { ErrorFallback } from "@/components/organisms/ErrorFallback";
-import type { TCategory, TConfig } from "@/types";
 
-type ErrorProps = InferGetStaticPropsType<typeof getStaticProps>;
-
-const ErrorPage: NextPage<ErrorProps> = ({ config, categories }) => (
-  <RootLayout config={config} categories={categories}>
-    <HtmlHeadNoIndex />
-    <ErrorFallback heading="Unhandled Error" message="サイト上で問題が発生しました。" />
-  </RootLayout>
-);
-
-type StaticProps = {
-  config: TConfig;
-  categories: TCategory[];
+type ErrorPageProps = {
+  pageTitle: string;
+  message: string;
 };
 
-export const getStaticProps: GetStaticProps<StaticProps> = async () => {
-  const config = await fetchConfig();
-  const categories = await fetchCategories();
+const ErrorPage: NextPage<ErrorPageProps> = ({ pageTitle, message }) => (
+  <>
+    <HtmlHeadNoIndex />
+    <ErrorFallback heading={pageTitle} message={message} />
+  </>
+);
+
+ErrorPage.getInitialProps = async (contextData) => {
+  await Sentry.captureUnderscoreErrorException(contextData);
+
+  const { err } = contextData;
+
+  let pageTitle;
+  let message;
+
+  switch (err?.statusCode) {
+    case 404:
+      pageTitle = "404 - Not Found";
+      message = "ページが見つかりませんでした";
+      break;
+    case 500:
+      pageTitle = "500 - Server Error";
+      message = "サーバーで問題が発生しました";
+      break;
+    default:
+      pageTitle = "Unhandled Error";
+      message = "サイト上で問題が発生しました";
+  }
 
   return {
-    props: {
-      config,
-      categories,
-    },
+    pageTitle,
+    message,
   };
 };
 
