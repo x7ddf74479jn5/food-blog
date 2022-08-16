5b8a52c2f5b07057eae530bef5919286eb5ba25d
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-16 16:04:01 +0900

    ♻️ refactor(#32): fetcher

--- src/api/fetchArticles.ts
+++ src/api/fetchArticles.ts
@@ -3,60 +3,35 @@ import type { MicroCMSQueries } from "microcms-js-sdk/dist/cjs/types";
 import { client } from "@/lib/microcms";
 import type { TArticle, TArticleListResponse } from "@/types";
 import type { TPickupListResponse } from "@/types/pickup";
-import { HttpError } from "@/utils/error/Http";
 
 export const fetchArticles = async (queries?: MicroCMSQueries): Promise<TArticleListResponse> => {
-  try {
-    const data = await client.get<TArticleListResponse>({
-      endpoint: "articles",
-      queries: { limit: 1000, ...queries },
-    });
-    return data;
-  } catch (error) {
-    if (error instanceof HttpError) {
-      console.error(error);
-      throw new Error("記事の取得に失敗しました。");
-    }
-    throw error;
-  }
+  const data = await client.get<TArticleListResponse>({
+    endpoint: "articles",
+    queries: { limit: 1000, ...queries },
+  });
+  return data;
 };
 
 export const fetchArticle = async (id: string, queries?: MicroCMSQueries): Promise<TArticle> => {
-  try {
-    const data = await client.get<TArticle>({
-      endpoint: `articles`,
-      contentId: id,
-      queries: {
-        depth: 2,
-        ...queries,
-      },
-    });
-    return data;
-  } catch (error) {
-    if (error instanceof HttpError) {
-      console.error(error);
-      throw new Error("記事の取得に失敗しました。");
-    }
-    throw error;
-  }
+  const data = await client.get<TArticle>({
+    endpoint: `articles`,
+    contentId: id,
+    queries: {
+      depth: 2,
+      ...queries,
+    },
+  });
+  return data;
 };
 
 export const fetchPickupArticles = async (queries: MicroCMSQueries) => {
-  try {
-    const data = await client.get<TPickupListResponse>({
-      endpoint: "pickups",
-      queries: {
-        orders: "-publishedAt",
-        depth: 2,
-        ...queries,
-      },
-    });
-    return data;
-  } catch (error) {
-    if (error instanceof HttpError) {
-      console.error(error);
-      throw new Error("記事の取得に失敗しました。");
-    }
-    throw error;
-  }
+  const data = await client.get<TPickupListResponse>({
+    endpoint: "pickups",
+    queries: {
+      orders: "-publishedAt",
+      depth: 2,
+      ...queries,
+    },
+  });
+  return data;
 };
--- src/api/fetchTags.ts
+++ src/api/fetchTags.ts
@@ -1,31 +1,14 @@
 import { client } from "@/lib/microcms";
 import type { TTag, TTagListResponse } from "@/types";
-import { HttpError } from "@/utils/error/Http";
 
 export const fetchTag = async (slug: string): Promise<TTag> => {
-  try {
-    const data = await client.get<TTagListResponse>({ endpoint: "tags", queries: { filters: `slug[equals]${slug}` } });
-    return data.contents[0] ?? [];
-  } catch (error) {
-    if (error instanceof HttpError) {
-      console.error(error);
-      throw new Error("タグ情報の取得に失敗しました。");
-    }
-    throw error;
-  }
+  const data = await client.get<TTagListResponse>({ endpoint: "tags", queries: { filters: `slug[equals]${slug}` } });
+  return data.contents[0] ?? [];
 };
 
 export const fetchTags = async (): Promise<TTag[]> => {
-  try {
-    const data = await client.get<TTagListResponse>({ endpoint: "tags", queries: { limit: 100 } });
-    return data.contents;
-  } catch (error) {
-    if (error instanceof HttpError) {
-      console.error(error);
-      throw new Error("タグ情報の取得に失敗しました。");
-    }
-    throw error;
-  }
+  const data = await client.get<TTagListResponse>({ endpoint: "tags", queries: { limit: 100 } });
+  return data.contents;
 };
 
 export default fetchTags;
