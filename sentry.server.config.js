4c93e83fa637d85184dd1da6863370c755e2400e
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-19 17:01:46 +0900

    🔧 update(#32): sentry config

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,5 +4,6 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
+  environment: process.env.NEXT_PUBLIC_APP_ENV,
   tracesSampleRate: 1.0,
 });
