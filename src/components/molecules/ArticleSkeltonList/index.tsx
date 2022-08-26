d64a42e20a3ace8233be7938498c7976d6381f03
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-08-26 19:16:29 +0900

    ♻️ refactor(#43): implement Skelton

--- src/components/molecules/ArticleSkeltonList/index.tsx
+++ src/components/molecules/ArticleSkeltonList/index.tsx
@@ -1,16 +1,11 @@
 import { ArticleSkelton } from "@/components/atoms/ArticleSkelton";
 
 export const ArticleSkeltonList: React.FC = () => {
-  const length = 5;
-  const list = [];
+  const lists = [...Array(5)].map((_, index) => (
+    <li key={index}>
+      <ArticleSkelton />
+    </li>
+  ));
 
-  for (let index = 0; index < length; index++) {
-    list.push(
-      <li key={index}>
-        <ArticleSkelton />
-      </li>
-    );
-  }
-
-  return <ul className="space-y-12">{list}</ul>;
+  return <ul className="space-y-12">{lists}</ul>;
 };
