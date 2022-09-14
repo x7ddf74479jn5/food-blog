f7fdad381ff133939abde839f5c96d8723e011f6
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-09-15 00:54:13 +0900

    ♻️ refactor(#42): CategoryMenu for readability

--- src/components/molecules/category/CategoryMenu/index.tsx
+++ src/components/molecules/category/CategoryMenu/index.tsx
@@ -9,12 +9,12 @@ import { classNames } from "@/utils/css";
 import { urlTable } from "@/utils/paths/url";
 
 type CategoryMenuItemProps = {
-  text: string;
+  label: string;
   href: string;
   disabled?: boolean;
 };
 
-export const CategoryMenuItem: React.FC<CategoryMenuItemProps> = ({ href, text, disabled }) => {
+export const CategoryMenuItem: React.FC<CategoryMenuItemProps> = ({ href, label, disabled }) => {
   return (
     <Menu.Item as="div" disabled={disabled}>
       {({ active }) => (
@@ -22,53 +22,19 @@ export const CategoryMenuItem: React.FC<CategoryMenuItemProps> = ({ href, text, 
           href={href}
           className={classNames("dropdown-option", active ? "dropdown-active" : "dropdown-inactive")}
         >
-          {text}
+          {label}
         </NextLink>
       )}
     </Menu.Item>
   );
 };
 
-type CategoryMenuItemsProps = {
-  open: boolean;
-  categories: TCategory[];
-};
+const usePath = () => {
+  const { asPath } = useRouter();
 
-const CategoryMenuItems: React.FC<CategoryMenuItemsProps> = ({ open, categories }) => {
-  const router = useRouter();
-  const asPath = router.asPath;
+  const matchPath = (path: string) => asPath === path;
 
-  const isCurrentPage = (slug: string) => {
-    if (!asPath.startsWith(urlTable.categories)) {
-      return false;
-    }
-    if (slug === router.query.slug) {
-      return true;
-    }
-    return false;
-  };
-
-  return (
-    <>
-      {open && (
-        <Menu.Items static className="dropdown-options w-28">
-          <CategoryMenuItem text="一覧" href={urlTable.categories} disabled={asPath === urlTable.categories} />
-
-          <CategoryMenuItem text="おすすめ" href={urlTable.pickup} disabled={asPath === urlTable.pickup} />
-          <CategoryMenuItem text="人気" href={urlTable.popular} disabled={asPath === urlTable.popular} />
-
-          {categories.map((category) => (
-            <CategoryMenuItem
-              key={category.id}
-              href={`${urlTable.categories}/${category.slug}`}
-              text={category.name}
-              disabled={isCurrentPage(category.slug)}
-            />
-          ))}
-        </Menu.Items>
-      )}
-    </>
-  );
+  return { matchPath };
 };
 
 type CategoryMenuProps = {
@@ -76,6 +42,8 @@ type CategoryMenuProps = {
 };
 
 export const CategoryMenu: React.FC<CategoryMenuProps> = memo(({ categories }) => {
+  const { matchPath } = usePath();
+
   return (
     <Menu as="div" className="dropdown">
       {({ open }) => (
@@ -85,7 +53,23 @@ export const CategoryMenu: React.FC<CategoryMenuProps> = memo(({ categories }) =
             {open ? <FaChevronUp /> : <FaChevronDown />}
           </Menu.Button>
 
-          <CategoryMenuItems open={open} categories={categories} />
+          {open && (
+            <Menu.Items static className="dropdown-options w-28">
+              <CategoryMenuItem label="一覧" href={urlTable.categories} disabled={matchPath(urlTable.categories)} />
+
+              <CategoryMenuItem label="おすすめ" href={urlTable.pickup} disabled={matchPath(urlTable.pickup)} />
+              <CategoryMenuItem label="人気" href={urlTable.popular} disabled={matchPath(urlTable.popular)} />
+
+              {categories.map((category) => (
+                <CategoryMenuItem
+                  key={category.id}
+                  href={`${urlTable.categories}/${category.slug}`}
+                  label={category.name}
+                  disabled={matchPath(`${urlTable.categories}/${category.slug}`)}
+                />
+              ))}
+            </Menu.Items>
+          )}
         </>
       )}
     </Menu>
