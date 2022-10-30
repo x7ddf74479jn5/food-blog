a99b386bf82f9ba3e9da7a0b18fe9b3f6152bf03
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-10-31 01:05:19 +0900

    ♻️ refactor: codemod next/link

--- src/components/atoms/NextLink/index.tsx
+++ src/components/atoms/NextLink/index.tsx
@@ -3,19 +3,14 @@ import Link from "next/link";
 type Props = {
   href: string;
   className?: string;
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   "aria-label"?: string;
   onClick?: () => void;
   children?: React.ReactNode;
   prefetch?: boolean;
 };
 
-const NextLink: React.FC<Props> = ({ href, children, prefetch = false, ...rest }) => {
-  return (
-    <Link href={href} prefetch={prefetch}>
-      <a {...rest}>{children}</a>
-    </Link>
-  );
+const NextLink: React.FC<Props> = ({ children, prefetch = false, ...rest }) => {
+  return <Link prefetch={prefetch} {...rest}>{children}</Link>;
 };
 
 export default NextLink;
--- src/components/atoms/mdx/CustomLink/index.tsx
+++ src/components/atoms/mdx/CustomLink/index.tsx
@@ -4,14 +4,12 @@ import type { ComponentPropsWithoutRef, FC, PropsWithChildren } from "react";
 type CustomLinkProps = PropsWithChildren<ComponentPropsWithoutRef<"a">>;
 
 export const CustomLink: FC<CustomLinkProps> = ({ href = "", children }) => {
-  const isInternalLink = href.startsWith("/") ? true : false;
+  const isInternalLink = href.startsWith("/");
 
   return (
     <>
       {isInternalLink ? (
-        <Link href={href}>
-          <a>{children}</a>
-        </Link>
+        <Link href={href}>{children}</Link>
       ) : (
         <a href={href} target="_blank" rel="noopener noreferrer">
           {children}
