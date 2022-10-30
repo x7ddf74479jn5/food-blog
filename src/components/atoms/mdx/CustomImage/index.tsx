e10ffc03978bf426a1ea647ba43277eb6e04aa4e
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-10-31 00:56:34 +0900

    ♻️ refactor: codemod next/image

--- src/components/atoms/mdx/CustomImage/index.tsx
+++ src/components/atoms/mdx/CustomImage/index.tsx
@@ -1,4 +1,4 @@
-import Image from "next/future/image";
+import Image from "next/image";
 import type { ComponentPropsWithoutRef, FC } from "react";
 
 type CustomImageProps = ComponentPropsWithoutRef<"img">;
