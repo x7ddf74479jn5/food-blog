b9a5f135cb25953477fc1a32872b290d24dd7f05
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-11-21 01:33:24 +0900

    fixup! 🔧 update: Routerのモックを更新

--- jest/mocks/router.tsx
+++ jest/mocks/router.tsx
@@ -1,5 +1,18 @@
 import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import { AppRouterContext } from "next/dist/shared/lib/app-router-context";
+import {
+  LayoutSegmentsContext,
+  ParamsContext,
+  PathnameContext,
+  SearchParamsContext,
+} from "next/dist/shared/lib/hooks-client-context";
+
+type HooksClientOption = {
+  params: any;
+  searchParams: URLSearchParams | null;
+  pathname: string | null;
+  layoutSegments: any;
+};
 
 export const defaultMockRouter: AppRouterInstance = {
   back: jest.fn(),
@@ -10,17 +23,46 @@ export const defaultMockRouter: AppRouterInstance = {
   replace: jest.fn(),
 };
 
+const defaultMockHooksClient: HooksClientOption = {
+  layoutSegments: {},
+  params: {},
+  pathname: null,
+  searchParams: null,
+};
+
 export const withMockRouter = (
-  router: Partial<AppRouterInstance> = {},
+  {
+    options,
+    router,
+  }: {
+    options?: Partial<HooksClientOption>;
+    router?: Partial<AppRouterInstance>;
+  },
   children: React.ReactElement
 ): React.ReactElement => {
-  const mockedRouter: AppRouterInstance = {
+  const mockRouter: AppRouterInstance = {
     ...defaultMockRouter,
     ...router,
   };
+  const mockHooksClient = {
+    ...defaultMockHooksClient,
+    ...options,
+  };
 
-  return <AppRouterContext.Provider value={mockedRouter}>{children}</AppRouterContext.Provider>;
+  return (
+    <AppRouterContext.Provider value={mockRouter}>
+      <PathnameContext.Provider value={mockHooksClient.pathname}>
+        <ParamsContext.Provider value={mockHooksClient.params}>
+          <SearchParamsContext.Provider value={mockHooksClient.searchParams}>
+            <LayoutSegmentsContext.Provider value={mockHooksClient.layoutSegments}>
+              {children}
+            </LayoutSegmentsContext.Provider>
+          </SearchParamsContext.Provider>
+        </ParamsContext.Provider>
+      </PathnameContext.Provider>
+    </AppRouterContext.Provider>
+  );
 };
 
 export { AppRouterContext };
-export type { AppRouterInstance };
+export type { AppRouterInstance, HooksClientOption };
