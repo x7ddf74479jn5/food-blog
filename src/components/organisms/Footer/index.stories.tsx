feed4ece558dbbd27ee2767c7c9f678433930c36
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-11-22 21:18:38 +0900

    fixup! ♻️ refactor: Footer migration

--- src/components/organisms/Footer/index.stories.tsx
+++ src/components/organisms/Footer/index.stories.tsx
@@ -3,8 +3,8 @@ import type { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 import { FooterView } from "./FooterView";
 
 export default {
-  title: "organisms/Footer",
   component: FooterView,
+  title: "organisms/Footer",
 } as ComponentMeta<typeof FooterView>;
 
 export const Default: ComponentStoryObj<typeof FooterView> = {
