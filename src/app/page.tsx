d95e1fd0cef7ac3b1c5715c76a0ce00decf827bf
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-11-22 21:40:47 +0900

    ♻️ refactor: Home page migration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,8 @@
-import type { GetStaticProps, NextPage } from "next";
-
-import ErrorPage from "@/app/_error/page";
-import type { HomeProps } from "@/components/pages/Home";
 import { Home } from "@/components/pages/Home";
-import { sentryLogServer } from "@/lib/sentry/logger";
-import { fetchConfig, fetchTags } from "@/repositories";
-import { getArticles, getPickupArticles, getPopularArticles } from "@/services/article";
-import { getCategories } from "@/services/category";
-import type { PagePropsOrError } from "@/types";
-import { generatedRssFeed } from "@/utils/rss/rss";
 
-type HomePageProps = PagePropsOrError<HomeProps>;
-
-const HomePage: NextPage<HomePageProps> = (props) => {
-  return props.error ? <ErrorPage statusCode={props.error.statusCode} /> : <Home {...props} />;
+const HomePage = async () => {
+  // @ts-expect-error server component
+  return <Home />;
 };
 
 export default HomePage;
-
-export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
-  try {
-    const [config, categories, tags, data, pickup, popularArticles] = await Promise.all([
-      fetchConfig(),
-      getCategories(),
-      fetchTags(),
-      getArticles({ limit: 10, offset: 0 }),
-      getPickupArticles(new Date()),
-      getPopularArticles(),
-    ]);
-
-    if (process.env.NODE_ENV === "production") {
-      generatedRssFeed(config, data.contents);
-    }
-
-    return {
-      props: {
-        categories,
-        config,
-        data,
-        pickup,
-        popularArticles,
-        tags,
-      },
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      await sentryLogServer(error);
-    }
-
-    return {
-      props: {
-        error: {
-          statusCode: 500,
-        },
-      },
-    };
-  }
-};
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,35 +1,75 @@
-import { HtmlHeadBase } from "@/components/functions/meta";
-import { HomeLayout } from "@/components/layouts";
-import { ArticleSWRContainer } from "@/components/organisms/article";
-import type { TArticleListResponse, TCategory, TConfig, TPickup, TRankedArticle, TTag } from "@/types";
+import { FaLightbulb } from "react-icons/fa";
+import { IoPodium } from "react-icons/io5";
 
-export type HomeProps = {
-  data: TArticleListResponse;
-  categories: TCategory[];
-  tags: TTag[];
-  config: TConfig;
-  pickup: TPickup;
+import { AsideContainer, MainContainer, MiddleAreaContainer } from "@/components/atoms/containers";
+import { CategoryListSide } from "@/components/molecules/category/CategoryListSide";
+import { ShareButtons } from "@/components/molecules/ShareButtons";
+import { ArticleSWRContainer, CarouselContainer } from "@/components/organisms/article";
+import { PickupArticleCarousel } from "@/components/organisms/article/carousel/PickupArticleCarousel";
+import { PopularArticleCarousel } from "@/components/organisms/article/carousel/PopularArticleCarousel";
+import { fetchConfig } from "@/repositories";
+import { getArticles, getPickupArticles, getPopularArticles } from "@/services/article";
+import type { TPickup, TRankedArticle } from "@/types";
+
+export const composeCarouselItems = ({
+  pickup,
+  popularArticles,
+}: {
+  pickup?: TPickup;
   popularArticles: TRankedArticle[];
+}) => {
+  const hasPickup = pickup && pickup.articles.length > 0;
+  const hasPopularArticles = popularArticles.length > 0;
+  const items = [];
+  if (hasPickup) {
+    items.push({
+      Carousel: <PickupArticleCarousel pickup={pickup} />,
+      Icon: <FaLightbulb className="h-4 w-4 text-yellow-400" />,
+      description: pickup?.description,
+      id: "pickup",
+      label: "PICKUP",
+    });
+  }
+
+  if (hasPopularArticles) {
+    items.push({
+      Carousel: <PopularArticleCarousel articles={popularArticles} />,
+      Icon: <IoPodium className="h-4 w-4 text-yellow-400" />,
+      description: "人気記事ランキング",
+      id: "popular",
+      label: "POPULAR",
+    });
+  }
+
+  return items;
 };
 
-export const Home: React.FC<HomeProps> = ({ categories, config, data, pickup, popularArticles, tags }) => {
-  const { host, siteTitle: title } = config;
+export const Home = async () => {
+  const fallbackData = await getArticles({ limit: 10, offset: 0 });
+  const [config, pickup, popularArticles] = await Promise.all([
+    fetchConfig(),
+    getPickupArticles(new Date()),
+    getPopularArticles(),
+  ]);
+  const { host, siteTitle } = config;
+  const carouselItems = composeCarouselItems({ pickup, popularArticles });
 
   return (
-    <HomeLayout
-      pickup={pickup}
-      url={host}
-      pageTitle={title}
-      config={config}
-      categories={categories}
-      tags={tags}
-      popularArticles={popularArticles}
-    >
-      <HtmlHeadBase indexUrl={host} siteTitle={title} />
-      <div className="mb-8">
-        <h1>レシピ一覧</h1>
-      </div>
-      <ArticleSWRContainer fallbackData={data} />
-    </HomeLayout>
+    <>
+      {carouselItems.length > 0 && <CarouselContainer items={carouselItems} />}
+      <MiddleAreaContainer>
+        <AsideContainer className="order-2 md:order-1">
+          <ShareButtons url={host} title={siteTitle} className="flex-row md:flex-col" />
+        </AsideContainer>
+        <MainContainer className="order-1 md:order-2">
+          <h1 className="mb-8">レシピ一覧</h1>
+          <ArticleSWRContainer fallbackData={fallbackData} />
+        </MainContainer>
+        <AsideContainer className="order-3">
+          {/* @ts-expect-error  server component */}
+          <CategoryListSide columns="grid-cols-3 sm:grid-cols-5 md:grid-cols-1" />
+        </AsideContainer>
+      </MiddleAreaContainer>
+    </>
   );
 };
