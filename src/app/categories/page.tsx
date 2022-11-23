ca10ed7616c1cf2efc2e66a2d29ce51fe6dc505c
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-11-23 22:44:53 +0900

    ♻️ refactor: categories page migration

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,3 @@
+import { Categories } from "@/components/pages/articles/categories/Categories";
+
+export default Categories;
--- src/components/pages/articles/categories/Categories/index.tsx
+++ src/components/pages/articles/categories/Categories/index.tsx
@@ -1,37 +1,46 @@
-import { TwoColumnLayout } from "@/components/layouts";
-import { CategoryList } from "@/components/molecules/category/CategoryList";
-import type { TCategory, TConfig, TPickup, TRankedArticle, TTag } from "@/types";
+import BackLinks from "@/components/feature/BackLinks";
+import { ShareButtons } from "@/components/feature/ShareButtons";
+import { PickupArticles, PopularArticles } from "@/components/model/article";
+import { CategoryList } from "@/components/model/category/CategoryList";
+import {
+  AsideContainer,
+  BottomAreaContainer,
+  ContainerWithOrder,
+  MiddleAreaContainer,
+} from "@/components/ui/containers";
+import { fetchConfig } from "@/repositories";
 import { formatPageTitle, formatPageUrl } from "@/utils/formatter";
 import { getBackLinks, urlTable } from "@/utils/paths/url";
 
-export type CategoriesProps = {
-  config: TConfig;
-  categories: TCategory[];
-  tags: TTag[];
-  pickup: TPickup;
-  popularArticles: TRankedArticle[];
-};
-
-export const Categories: React.FC<CategoriesProps> = ({ categories, config, pickup, popularArticles, tags }) => {
-  const { host, siteTitle } = config;
-  const heading = "カテゴリー一覧";
-  const pageTitle = formatPageTitle(heading, siteTitle);
+export const Categories = async () => {
+  const { host, siteTitle } = await fetchConfig();
+  const pageTitle = formatPageTitle("カテゴリー一覧", siteTitle);
   const url = formatPageUrl(urlTable.categories, host);
   const backLinks = getBackLinks([urlTable.home]);
+
   return (
-    <TwoColumnLayout
-      config={config}
-      pickup={pickup}
-      host={host}
-      url={url}
-      title={pageTitle}
-      backLinks={backLinks}
-      heading={heading}
-      categories={categories}
-      tags={tags}
-      popularArticles={popularArticles}
-    >
-      <CategoryList categories={categories} width={128} height={128} />
-    </TwoColumnLayout>
+    <>
+      <h1 className="my-8">カテゴリー一覧</h1>
+      <MiddleAreaContainer>
+        <AsideContainer className="order-2 md:order-1 lg:w-full">
+          <ShareButtons url={url} title={pageTitle} className="flex-row md:flex-col" />
+        </AsideContainer>
+        <main className="order-1 md:order-2">
+          {/* @ts-expect-error server component */}
+          <CategoryList width={128} height={128} />
+        </main>
+      </MiddleAreaContainer>
+      <BottomAreaContainer>
+        <ContainerWithOrder order="order-1 lg:order-2" className="grow">
+          {/* @ts-expect-error server component */}
+          <PickupArticles />
+          {/* @ts-expect-error server component */}
+          <PopularArticles />
+        </ContainerWithOrder>
+        <ContainerWithOrder order="order-2 lg:order-1" className="shrink">
+          <BackLinks links={backLinks} />
+        </ContainerWithOrder>
+      </BottomAreaContainer>
+    </>
   );
 };
