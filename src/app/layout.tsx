6222b9cb20340ec94f65281592c2653b5d3e8cd3
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-11-26 23:32:13 +0900

    🐛 fix: Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,18 +9,17 @@ import { RootProvider } from "./provider";
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
-      <RootProvider>
-        <body
-          className="mx-auto mb-16 flex min-h-screen max-w-screen-xl flex-col bg-white px-4 text-gray-900 dark:bg-gray-900 dark:text-gray-100"
-          style={{ overflowAnchor: "none" }}
-        >
-          {/* @ts-expect-error Server Component */}
-          <Header />
-          {children}
-          {/* @ts-expect-error Server Component */}
-          <Footer />
-        </body>
-      </RootProvider>
+      <body style={{ overflowAnchor: "none" }}>
+        <RootProvider>
+          <div className="mx-auto mb-16 flex min-h-screen max-w-screen-xl flex-col bg-white px-4 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+            {/* @ts-expect-error Server Component */}
+            <Header />
+            {children}
+            {/* @ts-expect-error Server Component */}
+            <Footer />
+          </div>
+        </RootProvider>
+      </body>
       <GoogleAnalytics />
     </html>
   );
