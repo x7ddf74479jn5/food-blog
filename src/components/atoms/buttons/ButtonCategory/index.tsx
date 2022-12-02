6d93e62efb7331365008ffbbd13fb9b9178ad589
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-02 18:42:33 +0900

    :bug: fix: ボタンカラーが一部しか適用されていない問題を修正

--- src/components/atoms/buttons/ButtonCategory/index.tsx
+++ src/components/atoms/buttons/ButtonCategory/index.tsx
@@ -1,6 +1,6 @@
 import NextLink from "@/components/atoms/NextLink";
 import type { TCategory } from "@/types";
-import { getBorderColor } from "@/utils/css";
+import { classNames } from "@/utils/css";
 import { urlTable } from "@/utils/paths/url";
 
 type ButtonCategoryProps = {
@@ -8,10 +8,28 @@ type ButtonCategoryProps = {
 };
 
 export const ButtonCategory: React.FC<ButtonCategoryProps> = ({ category }) => {
-  const borderColor = getBorderColor(category.color);
   return (
     <NextLink href={`${urlTable.categories}/${category.slug}`}>
-      <div className={`inline-block rounded-lg border-2 py-1 px-2 text-black dark:text-white ${borderColor}`}>
+      <div
+        className={classNames(
+          "inline-block rounded-lg border-2 py-1 px-2 text-black dark:text-white",
+          category.color === "blue"
+            ? "bg-blue-700 dark:border-blue-300"
+            : category.color === "green"
+            ? "bg-green-700 dark:border-green-300"
+            : category.color === "indigo"
+            ? "bg-indigo-700 dark:border-indigo-300"
+            : category.color === "pink"
+            ? "bg-pink-700 dark:border-pink-300"
+            : category.color === "purple"
+            ? "bg-purple-700 dark:border-purple-300"
+            : category.color === "red"
+            ? "bg-red-700 dark:border-red-300"
+            : category.color === "yellow"
+            ? "bg-yellow-700 dark:border-yellow-300"
+            : "bg-gray-700 dark:border-gray-300"
+        )}
+      >
         {category.name}
       </div>
     </NextLink>
--- src/utils/css/index.ts
+++ src/utils/css/index.ts
@@ -1,2 +1,1 @@
 export { classNames } from "./classNames";
-export { getBGColor, getBorderColor } from "./getColor";
