7f836a769a1207e76440983400f66d134b5f393e
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-02 16:55:22 +0900

    ♻️ refactor: Avatarの視認性を改善

--- src/components/atoms/Avatar/index.tsx
+++ src/components/atoms/Avatar/index.tsx
@@ -9,6 +9,6 @@ type AvatarProps = {
 export const Avatar: React.FC<AvatarProps> = (props) => {
   return (
     // eslint-disable-next-line jsx-a11y/alt-text
-    <Image {...props} className="m-0 rounded-full" />
+    <Image {...props} className="m-0 rounded-full border border-gray-100 bg-pink-700" />
   );
 };
