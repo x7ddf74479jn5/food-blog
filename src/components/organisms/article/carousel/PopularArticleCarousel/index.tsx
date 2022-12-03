8843ca2b80d311cb94301bbea6e775ce33f65bd9
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-04 00:33:09 +0900

    :recylce: refactor: ♻️ refactor: 人気記事カルーセルの表示を改善

--- src/components/organisms/article/carousel/PopularArticleCarousel/index.tsx
+++ src/components/organisms/article/carousel/PopularArticleCarousel/index.tsx
@@ -14,7 +14,7 @@ export const PopularArticleCarousel: React.FC<PopularArticleCarouselProps> = mem
       {articles.map((article) => (
         <article className="relative max-h-48 w-full min-w-full overflow-hidden pl-2 sm:min-w-[50%]" key={article.id}>
           <Thumbnail src={article.image.url} title={article.title} id={article.id} blurDataURL={article.image.url} />
-          <div className="absolute top-0 left-0 bg-black/25 p-2 text-sm font-semibold text-white">{article.order}</div>
+          <div className="absolute top-0 left-2 bg-black/25 p-2 text-sm font-semibold text-white">{article.order}</div>
           <h3 className="absolute bottom-3 block w-full truncate bg-black/25 p-1 text-sm font-semibold text-white">
             {article.title}
           </h3>
