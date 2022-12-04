22d01c9e08afce9d7a8d71eb0f720bbd930bbaf7
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-05 01:31:11 +0900

    📚 story: @next/fontで生成されたフォントの追加

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import "@/styles/global.css";
+import "./index.css";
 
 import { withPerformance } from "storybook-addon-performance";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
