538accc0eed5d12baee57f936cae48a0b3da6465
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-14 20:35:58 +0900

    :adhesive_bandage: chore: CategoryButtonの見た目

--- src/utils/css/color.ts
+++ src/utils/css/color.ts
@@ -18,16 +18,16 @@ export const getBGColor = (color: TCategoryOrTagColor) => {
 
 export const getBorderColor = (color: TCategoryOrTagColor) => {
   return color === "blue"
-    ? "bg-blue-700 dark:border-blue-300"
+    ? "border-blue-700 dark:border-blue-300"
     : color === "green"
-    ? "bg-green-700 dark:border-green-300"
+    ? "border-green-700 dark:border-green-300"
     : color === "pink"
-    ? "bg-pink-700 dark:border-pink-300"
+    ? "border-pink-700 dark:border-pink-300"
     : color === "purple"
-    ? "bg-purple-700 dark:border-purple-300"
+    ? "border-purple-700 dark:border-purple-300"
     : color === "red"
-    ? "bg-red-700 dark:border-red-300"
+    ? "border-red-700 dark:border-red-300"
     : color === "yellow"
-    ? "bg-yellow-700 dark:border-yellow-300"
-    : "bg-gray-700 dark:border-gray-300";
+    ? "border-yellow-700 dark:border-yellow-300"
+    : "border-gray-700 dark:border-gray-300";
 };
