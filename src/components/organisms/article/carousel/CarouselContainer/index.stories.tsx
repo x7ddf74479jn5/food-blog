7b4b29986cd27014248023fe0b13b7253d954ada
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-25 00:05:23 +0900

    🔧 config: storycap

--- src/components/organisms/article/carousel/CarouselContainer/index.stories.tsx
+++ src/components/organisms/article/carousel/CarouselContainer/index.stories.tsx
@@ -2,25 +2,21 @@ import type { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 import { mockPickup, mockPopularArticles } from "mocks/data";
 
 import { CarouselContainer } from ".";
-import { device } from ".storybook/mocks/device";
+import { screenshotViewportVariants } from ".storybook/screenshot";
 
 export default {
   component: CarouselContainer,
   title: "organisms/article/carousel/CarouselContainer",
 } as ComponentMeta<typeof CarouselContainer>;
 
-export const Desktop: ComponentStoryObj<typeof CarouselContainer> = {
+export const Default: ComponentStoryObj<typeof CarouselContainer> = {
   args: {
     pickup: mockPickup,
     popularArticles: mockPopularArticles,
   },
-};
-
-export const Mobile: ComponentStoryObj<typeof CarouselContainer> = {
-  ...Desktop,
   parameters: {
-    viewport: {
-      defaultViewport: device.mobile,
+    screenshot: {
+      variants: screenshotViewportVariants,
     },
   },
 };
